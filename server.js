2fa954d09fd4c37cef6d8817c9137b983da3d224
Yvour <ivour@rambler.ru>
2020-02-03 01:14:45 +0100

    removing unnecessary function

--- server.js
+++ server.js
@@ -21,23 +21,6 @@ app.use(
 
 app.use(express.static(__dirname + "/www"));
 
-function postData(url = "", data = {}) {
-  // Значения по умолчанию обозначены знаком *
-  return fetch(url, {
-    method: "POST", // *GET, POST, PUT, DELETE, etc.
-    mode: "cors", // no-cors, cors, *same-origin
-    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: "same-origin", // include, *same-origin, omit
-    headers: {
-      "Content-Type": "application/json"
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    redirect: "follow", // manual, *follow, error
-    referrer: "no-referrer", // no-referrer, *client
-    body: JSON.stringify(data) // тип данных в body должен соответвовать значению заголовка "Content-Type"
-  }).then(response => response.json()); // парсит JSON ответ в Javascript объект
-}
-
 app.get("/query-pixabay/:search", function(req, res) {
   const KEY = "15109459-ccff9a366d104475a924cc43e";
   const search = (req.params.search || "").split(/\s+/).join("+");
